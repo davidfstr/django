fef6494d2246d8988e7ba83e0827ed56d60608b5
David Foster <david@dafoster.net>
2019-09-27 10:10:25 -0700

    Convert middleware to be async: MessageMiddleware

--- django/contrib/messages/middleware.py
+++ django/contrib/messages/middleware.py
@@ -8,10 +8,10 @@ class MessageMiddleware(MiddlewareMixin):
     Middleware that handles temporary messages.
     """
 
-    def process_request(self, request):
+    async def process_request(self, request):
         request._messages = default_storage(request)
 
-    def process_response(self, request, response):
+    async def process_response(self, request, response):
         """
         Update the storage backend (i.e., save the messages).
 
@@ -20,6 +20,9 @@ class MessageMiddleware(MiddlewareMixin):
         # A higher middleware layer may return a request which does not contain
         # messages storage, so make no assumption that it will be there.
         if hasattr(request, '_messages'):
+            # TODO: Consider supporting third-party message storage backends
+            #       that expose an async interface. No built-in storages in
+            #       Django currently require an async interface.
             unstored_messages = request._messages.update(response)
             if unstored_messages and settings.DEBUG:
                 raise ValueError('Not all temporary messages could be stored.')
